34422198af89ee1cb8a93b1beac9acd2587d577e
weldoy <132596720+weldoy@users.noreply.github.com>
2024-12-11 13:07:33 +0300

    14, 6

--- main.py
+++ main.py
@@ -48,3 +48,52 @@
 #
 #
 # print(f(2024) - f(2021))
+
+# -------------------------------------------------
+
+# 14 ЗАДАНИЕ
+
+# numbers = []
+# for y in '01234567':
+#     for x in range(1, 8): # Не от нуля, тк x в начале стоит
+#         number = int(f'{x}01{y}4', 9) + int(f'{x}{y}544', 8)
+#         if number % 89 == 0:
+#             numbers.append(number)
+# print(min(numbers)//89)
+
+# -------------------------------------------------
+
+# 6 ЗАДАНИЕ
+
+# from turtle import *
+#
+# size = 10
+# tracer(100)
+#
+# lt(90)
+#
+# for i in range(2):
+#     fd(23 * size)
+#     rt(90)
+#     fd(10 * size)
+#     rt(90)
+#
+# fd(3 * size)
+# lt(90)
+# fd(12 * size)
+# rt(90)
+#
+# for i in range(2):
+#     fd(9 * size)
+#     rt(90)
+#     fd(32 * size)
+#     rt(90)
+#
+# up()
+#
+# for x in range(-20 * size, 30 * size, size):
+#     for y in range(-10 * size, 30 * size, size):
+#         goto(x, y)
+#         dot(2)
+#
+# exitonclick()
